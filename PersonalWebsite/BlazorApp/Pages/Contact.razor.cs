09f7ed4854ccf4bb61205701a625f3ccf5f21ddd
Gabriel Ioniță <gabrielionita@live.com>
2020-08-30 15:56:29 +0300

    Added contact page

---
+++ PersonalWebsite/BlazorApp/Pages/Contact.razor.cs
@@ -0,0 +1,34 @@
+﻿using BlazorApp.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Net.Http;
+
+namespace BlazorApp.Pages
+{
+	public partial class Contact : ComponentBase
+	{
+		private ContactModel contact;
+
+		[Inject]
+		private NavigationManager Navigation { get; set; }
+
+		[Inject]
+		private HttpClient HttpClient { get; set; }
+
+		protected override void OnInitialized()
+		{
+			OnReset();
+		}
+
+		private void OnReset()
+		{
+			contact = new ContactModel();
+		}
+
+		private void OnValidSubmit()
+		{
+			Console.WriteLine(HttpClient.BaseAddress.AbsoluteUri);
+			Navigation.NavigateTo("/");
+		}
+	}
+}
